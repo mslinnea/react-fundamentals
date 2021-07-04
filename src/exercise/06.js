653642b2c0dfa4dd9b9ca5f16e5c9566c4958447
Linnea Huxford <7308162+mslinnea@users.noreply.github.com>
2021-07-04 10:32:06 -0700

    6 extra credit 1 - use ref

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -4,19 +4,11 @@
 import * as React from 'react'
 
 function UsernameForm({onSubmitUsername}) {
-  // 🐨 add a submit event handler here (`handleSubmit`).
-  // 💰 Make sure to accept the `event` as an argument and call
-  // `event.preventDefault()` to prevent the default behavior of form submit
-  // events (which refreshes the page).
-  // 📜 https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault
-  //
-  // 🐨 get the value from the username input (using whichever method
-  // you prefer from the options mentioned in the instructions)
-  // 💰 For example: event.target.elements[0].value
-  // 🐨 Call `onSubmitUsername` with the value of the input
+  const usernameInputRef = React.createRef();
+
   function handleSubmit(event) {
       event.preventDefault();
-      onSubmitUsername(event.target.elements[0].value);
+      onSubmitUsername(usernameInputRef.current.value);
   }
 
   // 🐨 add the onSubmit handler to the <form> below
@@ -27,7 +19,7 @@ function UsernameForm({onSubmitUsername}) {
     <form onSubmit={handleSubmit}>
       <div>
         <label>Username:</label>
-        <input type="text" />
+        <input type="text" id="usernameInput" ref={usernameInputRef}/>
       </div>
       <button type="submit">Submit</button>
     </form>
